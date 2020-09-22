e38ab372e427ef4498a0f24799b02598e54f1193
Javier Palacios <javier.palacios.h@gmail.com>
2020-09-22 11:28:14 +0200

    ficing connection to heroku db

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import cors from 'cors';
 import routes from './database/routes';
 import db from './database/models';
 
+import configJson from './database/config/config';
+
+const env = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
+const config = configJson[env];
+
 // CONSTANTS
 const PORT = process.env.PORT || 3000;
 const ENV = process.env.ENV || 'NO_SECRET_KEY';
@@ -36,6 +41,9 @@ const auth = async () => {
 };
 
 app.listen(PORT, () => {
+  console.log('process.env.DATABASE_URL', process.env.DATABASE_URL);
+  console.log('config', config);
+  console.log('config.environment', config.environment === 'production');
   console.log(`Hello world app listening on port ${PORT}, on ${ENV} env`);
   auth();
 });
