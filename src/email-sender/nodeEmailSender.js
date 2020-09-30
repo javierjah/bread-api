298312b56495704d169f560e11f74cec9de37719
Javier Palacios <javier.palacios.h@gmail.com>
2020-10-01 00:55:48 +0200

    Trigger Heroku deploy

--- src/email-sender/nodeEmailSender.js
+++ src/email-sender/nodeEmailSender.js
@@ -33,12 +33,27 @@ function emailSender({ emailTo = EMAILS, subject = defaultSubject, emailParams }
     .then(async htmlTemplate => {
       const EMAIL = process.env.EMAIL;
       const EMAIL_PASS = process.env.EMAIL_PASS;
+      const OAUTH2_CLIENT_ID = process.env.OAUTH2_CLIENT_ID;
+      const OAUTH2_CLIENT_SECRET = process.env.OAUTH2_CLIENT_SECRET;
+      const OAUTH2_REFRESH_TOKEN = process.env.OAUTH2_REFRESH_TOKEN;
+      const OAUTH2_ACCESS_TOKEN = process.env.OAUTH2_ACCESS_TOKEN;
+
+      // const transporter = nodemailer.createTransport({
+      //   service: 'gmail',
+      //   auth: {
+      //     user: EMAIL,
+      //     pass: EMAIL_PASS,
+      //   },
+      // });
 
       const transporter = nodemailer.createTransport({
-        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
         auth: {
-          user: EMAIL,
-          pass: EMAIL_PASS,
+          type: 'OAuth2',
+          clientId: OAUTH2_CLIENT_ID,
+          clientSecret: OAUTH2_CLIENT_SECRET,
         },
       });
 
@@ -50,6 +65,12 @@ function emailSender({ emailTo = EMAILS, subject = defaultSubject, emailParams }
         subject,
         // HTML body
         html: htmlTemplate,
+        auth: {
+          user: EMAIL,
+          refreshToken: OAUTH2_REFRESH_TOKEN,
+          accessToken: OAUTH2_ACCESS_TOKEN,
+          expires: 3599,
+        },
       };
 
       transporter.sendMail(mailOptions, (emailError, info) => {
