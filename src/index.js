7481ece56b772d06cbfd12957a76ffa55520a60e
Javier Palacios <javier.palacios.h@gmail.com>
2020-10-14 19:57:54 +0200

    refactor: update cors. Test if swagger can do fetch in dev env

--- src/index.js
+++ src/index.js
@@ -22,7 +22,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 // app routes setup
 app.use(`${API_VERSION}/breads`, routes.bread);
 app.use(`${API_VERSION}/purchases`, routes.purchase);
